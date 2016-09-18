034a6fcac812d07a0b52c980003cdb964c9181c6
Macbook Air <macbookair@David-Ramirez.netgear.com>
2016-09-17 17:16:34 -0700

    add angular factoty function and value

---
+++ app/managers/dessert-manager.js
@@ -0,0 +1,20 @@
+angular.module('dessert', [])
+.value('DessertValues', {
+	pies: [
+		{flavor: "Cherry", score: 6},
+		{flavor: "Apple",score: 7.5},
+		{flavor: "Peach",score: 4}
+	]
+});
+
+angular.module('dessert').factory('DessertManager', 
+	['DessertValues',
+		function (dessertValues){
+			return {
+				pieFlavors: function(){
+					return dessertValues.pies.map(function(pie){
+						return pie.flavor;
+					});
+				}
+			};
+		}]);
